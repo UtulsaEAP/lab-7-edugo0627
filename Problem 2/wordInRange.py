e51d85a8834451080b7dfcbc206a7ce156b196b8
edugo0627 <ejv0065@utulsa.edu>
2024-03-28 13:47:40 -0500

    welp

--- Problem 2/wordInRange.py
+++ Problem 2/wordInRange.py
@@ -6,7 +6,18 @@ lab:@2pm
 
 def wordInRange():
     #Type your code here
+    file_name = input()
+    lower_word = input()
+    upper_word = input()
     
+    with open(file_name) as file_object:
+        lines = file_object.readlines()
+        for line in lines:
+            l =line.strip('\n')
+            if (l >= lower_word) and (l <= upper_word):
+                print(line.strip() + " - in range")
+            else:
+                print(line.strip() + " - not in range")
     return
 if __name__ == '__main__':
     wordInRange()
--- Problem 3/courseGrade.py
+++ Problem 3/courseGrade.py
@@ -1,12 +1,48 @@
 def courseGrade():
     
     # TODO: Declare any necessary variables here. 
+    filename = input()
+    newfile = 'report.text'
+    
+    midterm_1 = 0
+    midterm_2 = 0
+    finalt = 0
       
       
-    # TODO: Read a file name from the user and read the tsv file here. 
-   
-   
+    # TODO: Read a file name from the user and read the tsv file here.
+    with open (filename, 'r') as file:
+        data = file.readlines()
+        
+        for i in data:
+            colums = data.split('\t')
+            last_name = colums[0]
+            first_name = colums[1]
+            mt1 = int(colums[2])
+            midterm_1 += mt1
+            mt2 = int(colums[3])
+            midterm_2 += mt2
+            final = int(colums[4])
+            finalt += final
+           
+        
     # TODO: Compute student grades and exam averages, then output results to a text file here. 
+    avg = (mt1 +mt2 + final)/3
+    if avg >= 90:
+        letter = 'A'
+    elif avg >= 80:
+        letter = 'B'
+    elif avg >= 70:
+        letter = 'C'
+    elif avg >= 60:
+        letter = 'D'
+    else:
+        letter = 'F'
+        print(last_name,first_name,mt1,mt2,final,letter)
+     
+    mid1_avg = midterm_1/len(file)
+    mid2_avg = midterm_2/len(file)
+    final_avg = finalt/len(file)
+    print("Averages: midterm1 {:.2f}, midterm2 {:.2f}, final {:.2f}".format(mid1_avg,mid2_avg,final_avg))
     return
 
 if __name__ == "__main__":
